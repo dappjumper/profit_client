d272345b43c8bb4178e2fca748b25be387cea168
dappjumper <dappjump@gmail.com>
2020-10-18 18:51:02 +0200

    Dist

--- pages/app/bots/_bot.vue
+++ pages/app/bots/_bot.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container>
-    <BotDetails :botID="botID"/>
+    <pre v-if="bot">
+      {{ bot }}
+    </pre>
   </v-container>
 </template>
 
 <script>
-
-import BotDetails from './../../../components/BotDetails'
-
+import { botLogic } from './../../../mixins/botLogic'
 export default {
   name: 'BotDetailPage',
   layout: 'app',
+  mixins: [botLogic],
   computed: {
     botID () {
       return this.$route.params.bot
