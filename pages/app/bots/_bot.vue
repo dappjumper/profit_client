3050d0423d12fe4c1ec3beb57a4ee07dc475b685
dappjumper <dappjump@gmail.com>
2020-10-18 17:10:56 +0200

    debug

--- pages/app/bots/_bot.vue
+++ pages/app/bots/_bot.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <BotDetails :botID="$route.params.bot"/>
+    <BotDetails v-if="ready" :botID="$route.params.bot"/>
   </section>
 </template>
 
@@ -11,8 +11,17 @@ import BotDetails from './../../../components/BotDetails'
 export default {
   name: 'BotDetailPage',
   layout: 'app',
+  data () {
+    return {
+      ready: false
+    }
+  },
   components: {
     BotDetails
+  },
+  mounted () {
+    this.ready = true
+    console.log(this.$route.params.bot)
   }
 }
 </script>
